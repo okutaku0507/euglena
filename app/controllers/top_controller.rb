b6180e4ee303973a4b82cd95ddacca35d8850150
Takuya Okuhara <okutaku0507@gmail.com>
2015-07-08 03:26:57 +0900

    微調整

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,6 +3,6 @@ class TopController < ApplicationController
   end
   
   def growth
-    @organisms = [ Organism.where(name: 'euglena').first ] #Organism.order('id desc')
+    @organisms = [ Organism.where(description: 'euglena').first ] #Organism.order('id desc')
   end
 end
